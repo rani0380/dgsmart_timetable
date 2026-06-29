2ccfa3f3de7bb2b6573115c2e606b757e833ee8d
rani0380 <rani0380@users.noreply.github.com>
2026-06-29 14:13:47 +0900

    Update Apps Script web app URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.TIMETABLE_CONFIG = {
-  googleSheetWebAppUrl: "https://script.google.com/a/macros/dge.go.kr/s/AKfycbweQhzkk6ZWY7aHG40uP5SUqYyK5-N7wEPOZv7JgxPjktln4nRxLEzG18MnC0dt2ZuU/exec",
+  googleSheetWebAppUrl: "https://script.google.com/macros/s/AKfycbweQhzkk6ZWY7aHG40uP5SUqYyK5-N7wEPOZv7JgxPjktln4nRxLEzG18MnC0dt2ZuU/exec",
 };
